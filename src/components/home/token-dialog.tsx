3dc5fbf4341dd29f02aea005b3e1161a70fc0b72
RajuGangitla <gangitlaraju8520@gmail.com>
2024-07-18 12:51:41 +0530

    add laoder

--- src/components/home/token-dialog.tsx
+++ src/components/home/token-dialog.tsx
@@ -12,7 +12,7 @@ import { getAllSupportedChainsApi } from "@/services/token"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { useState } from "react"
 import GetAllToken from "../chains/get-token"
-import { ChevronDown } from "lucide-react"
+import { ChevronDown, Loader } from "lucide-react"
 import { Chain } from "@/types/chain"
 import useQuoteStore from "@/store/quote-store"
 import { SRC_TYPE } from "@/constants/quote"
@@ -48,19 +48,32 @@ export default function TokenDialog({ type }: ITokenDialog) {
                         <DialogDescription>Select the chain to view its tokens.</DialogDescription>
                     </DialogHeader>
                     <ScrollArea className="h-[300px] rounded-md border p-4">
-                        <div className="flex flex-wrap gap-4 py-2">
-                            {/* Supported Chains */}
-                            {getAllSupportedChains?.data?.supportedChains?.length > 0 &&
-                                getAllSupportedChains?.data?.supportedChains?.map((chain: Chain) => (
-                                    <button
-                                        key={chain.chainId}
-                                        onClick={() => updateChain(chain)}
-                                        className={`flex items-center justify-center rounded-md w-28 h-28 p-4 bg-muted hover:bg-accent cursor-pointer transition ${chain.chainId === selectedChain.chainId ? "border-2 border-white" : ""}`}
-                                    >
-                                        <p className="capitalize text-xs text-center">{chain?.name}</p>
-                                    </button>
-                                ))}
-                        </div>
+                        {
+                            isLoadinggetAllSupportedChains ? (
+                                <>
+                                    <div className="h-full flex items-center justify-center">
+                                        <Loader className="animate-spin text-blue-500" size={24} />
+                                    </div>
+                                </>
+                            ) : (
+                                <>
+                                    <div className="flex flex-wrap gap-4 py-2">
+                                        {/* Supported Chains */}
+                                        {getAllSupportedChains?.data?.supportedChains?.length > 0 &&
+                                            getAllSupportedChains?.data?.supportedChains?.map((chain: Chain) => (
+                                                <button
+                                                    key={chain.chainId}
+                                                    onClick={() => updateChain(chain)}
+                                                    className={`flex items-center justify-center rounded-md w-28 h-28 p-4 bg-muted hover:bg-accent cursor-pointer transition ${chain.chainId === selectedChain.chainId ? "border-2 border-white" : ""}`}
+                                                >
+                                                    <p className="capitalize text-xs text-center">{chain?.name}</p>
+                                                </button>
+                                            ))}
+                                    </div>
+                                </>
+                            )
+                        }
+
                     </ScrollArea>
                     <div className="mt-4">
                         {/* All Tokens */}
